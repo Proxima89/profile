00da691a735421c8512ffdaeff6f4522a6829ad9
Viktor Emilov <kiwispooon@gmail.com>
2021-02-25 16:05:31 +0000

    classfunctions

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,14 +1,38 @@
 class Gadget 
     def initialize 
         @username = "User #{rand(1..100)}"
-        @password = "topsecret"
+        @password  = "topsecret"
         @prod_number = "#{("a".."z").to_a.sample} --#{rand(1..999)}"
+    end
+
+    def to_s 
+        "Gadget #{@prod_number} has the username #{@username}."
+    end
+
+    def username 
+        @username
+    end
+
+    def username=(username) 
+        @username = username
+    end
+
+    def prod_number 
+        @prod_number
+    end
+
+    def password=(new_pass) 
+        @password = new_pass
     end
 end
 
 
 phone = Gadget.new 
-p phone.instance_variables
-
 laptop = Gadget.new
-p laptop.instance_variables
+
+p phone.password=("bestpassever")
+
+p phone.username
+phone.username=("Viktor")
+
+p phone.username
